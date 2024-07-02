057673a32e1d85c459876c9e6182385bd4c07081
Trung Tuan <tuandtse172284@fpt.edu.vn>
2024-07-02 12:11:43 +0700

    Detail shop app UI

---
+++ app/src/main/java/com/example/prm392/Activity/DetailActivity.java
@@ -0,0 +1,22 @@
+package com.example.prm392.Activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.prm392.R;
+
+public class DetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_detail);
+
+    }
+}
